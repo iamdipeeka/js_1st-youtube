1c51a1a404dd67d41af62e9586a8b6afb39d55f3
iamdipeeka <152793823+iamdipeeka@users.noreply.github.com>
2024-03-02 08:28:19 +0000

    variables in

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 12256
+let accountEmail = "dipeekaban@gmail.com"
+var accountPassword = "12335"
+accountCity = "jaipur"
+
+//accountId =2 not allowed to change
+
+accountEmail = "deep@gmmail.com"
+accountPassword ="22355"
+accountCity = "bangluru"
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
