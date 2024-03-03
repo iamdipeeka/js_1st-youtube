bbc50bac00ca3b428766951809bcf43846600102
iamdipeeka <152793823+iamdipeeka@users.noreply.github.com>
2024-03-03 04:11:42 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";  // treat all code as newer version
+
+//alert(3+3)   //we are using nodejs not browser
+
+console.log("dipeeka");
+console.log(3+3);  //readiability maintained hona chaiye
+
+let name ="dipeeka";
+let age ="21";
+let isloggedIn = false;
+
+
+//number=>2 to power 53
+//bigint
+//string 
+//boolean
+//null (object datatype ) =>standalone value which is empty
+//undefined
+//symbol =>unique
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);
+
+
+
